9c7b2bf165bfe79f790a3991378eba05f5035aad
Krishna <krishnadk8203@gmail.com>
2024-02-21 02:45:21 +0530

    add image meta tag2

--- src/Components/Seo.js
+++ src/Components/Seo.js
@@ -21,7 +21,7 @@ export const SEO = ({ title, description, pathname, image, children }) => {
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
+      <meta name="og:image" content={seo.image} />
       {/* <meta name="twitter:creator" content={seo.twitterUsername} /> */}
       {/* <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" /> */}
       {children}
