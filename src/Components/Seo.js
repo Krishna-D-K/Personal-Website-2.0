0b0d95236b9b471f160de66aa936dfd7986ee197
Krishna <krishnadk8203@gmail.com>
2024-02-25 18:59:34 +0530

    add meta tags

--- src/Components/Seo.js
+++ src/Components/Seo.js
@@ -15,13 +15,17 @@ export const SEO = ({ title, description, pathname, image, children }) => {
   return (
     <>
       <title>{seo.title}</title>
-      {/* <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} /> */}
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
+      <meta property="twitter:image" content={seo.image} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
+      <meta name="og:title" content={seo.title} />
       <meta name="og:image" content={seo.image} />
+      <meta name="og:url" content={seo.url} />
+      <meta name="og:description" content={seo.description} />
       {/* <meta name="twitter:creator" content={seo.twitterUsername} /> */}
       {/* <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" /> */}
       {children}
